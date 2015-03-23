eba8f0c7a72f5c0ac245bb98708cc53c741fc74c
Brett Hodges <bhodges87@gmail.com>
2015-03-23 16:49:26 -0400

    Initial Commit

---
+++ EmployeeInheritance/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EmployeeInheritance
+//
+//  Created by Brett Hodges on 3/23/15.
+//  Copyright (c) 2015 Brett Hodges. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
